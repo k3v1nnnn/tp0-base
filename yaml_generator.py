bb164a7df855934660febb6286e573ca72877055
Kevin Mendoza <kmendoza@fi.uba.ar>
2026-03-15 18:15:38 +0000

    Feat: Agrego sh y py

---
+++ yaml_generator.py
@@ -0,0 +1,52 @@
+import sys
+def client(id):
+    return f"""\
+  client{id}:
+    container_name: client{id}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID={id}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+"""
+
+def yaml(n_clients):
+    server = """\
+name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+"""
+    clients = "\n".join(client(i) for i in range(1, n_clients + 1))
+    networks = """\
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+"""
+    return server + "\n" + clients + "\n" + networks
+
+def main():
+    if len(sys.argv) == 3:
+        file = sys.argv[1]
+        n_clients = int(sys.argv[2])
+        if n_clients > 0:
+            yaml_file = yaml(n_clients)
+            with open(file, "w") as f:
+                f.write(yaml_file)
+
+if __name__ == "__main__":
+    main()
